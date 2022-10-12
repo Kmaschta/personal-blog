0cf36e417e95298a380eb135d07699f1a98b4591
Kmaschta <kevin@tint.ai>
2022-10-12 22:30:32 +0200

    Highlight code

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/a11y-dark.css';
+
 import Markdown from 'react-markdown-it';
 import Container from '../../components/Container/Container';
 
 import { getPostBySlug } from '../../lib/posts';
 
 export default function Post({ content, metadata }) {
+    useEffect(() => {
+        document.querySelectorAll('code[class]').forEach((el) => {
+            hljs.highlightElement(el);
+        });
+    }, []);
+
     return (
         <Container text>
             <Markdown source={content} options={{ html: true }} />
