87889c4fdeb71cae7c8606876af8a40d47801b08
Kmaschta <kevin@tint.ai>
2022-10-15 23:50:42 +0200

    Post title

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
+import Image from 'next/image';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/a11y-dark.css';
-
 import Markdown from 'react-markdown-it';
+
 import Container from '../../components/Container/Container';
 
 import { getAllPostSlugs, getPostBySlug } from '../../lib/posts';
+import styles from './Post.module.css';
 
-export default function Post({ content, metadata }) {
+export default function Post({ content, post }) {
     useEffect(() => {
         document.querySelectorAll('code[class]').forEach((el) => {
             hljs.highlightElement(el);
@@ -15,9 +17,29 @@ export default function Post({ content, metadata }) {
     }, []);
 
     return (
-        <Container text>
-            <Markdown source={content} options={{ html: true }} />
-        </Container>
+        <>
+            <div className={styles.postHeader}>
+                <Image
+                    src={post.image}
+                    alt=""
+                    layout="fill"
+                    objectFit="cover"
+                />
+                <div className={styles.postTitleContainer}>
+                    <Container text>
+                        <h1>{post.title}</h1>
+                        <div>
+                            {new Intl.DateTimeFormat('en-US', {
+                                dateStyle: 'medium',
+                            }).format(new Date(post.date))}
+                        </div>
+                    </Container>
+                </div>
+            </div>
+            <Container text>
+                <Markdown source={content} options={{ html: true }} />
+            </Container>
+        </>
     );
 }
 
@@ -35,6 +57,6 @@ export async function getStaticProps(context) {
     const { content, metadata } = getPostBySlug(slug);
 
     return {
-        props: { content, metadata },
+        props: { content, post: metadata },
     };
 }
