2647af1c75fa69519afc1ebb356a2425accd9c63
Kmaschta <kevin@tint.ai>
2023-01-04 18:45:26 +0100

    Allow SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Container from '../components/Container/Container';
 import Footer from '../components/Footer/Footer';
 import '../components/globals.css';
 import Header from '../components/Header/Header';
